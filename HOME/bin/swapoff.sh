db1f4aefcc024324f5fd283431ef50a284437d60
Petr Stepanov <stepanovps@gmail.com>
2021-11-04 15:40:05 -0400

    Autostart delay fixes

--- HOME/bin/swapoff.sh
+++ HOME/bin/swapoff.sh
@@ -1,13 +1,8 @@
 #/bin/bash
 
-if [ $auth = 0 ]; then
-    notify-send "Swap is disabled"
-fi
-
-echo "Disabling swap in a loop"
 while :
 do
-	echo "Press CTRL+C to exit"
 	swapoff -a
-	sleep 10
+	echo "Swap is disabled."
+        sleep 10
 done
--- install-swapoff.sh
+++ install-swapoff.sh
@@ -12,7 +12,13 @@ echo "Type=Application" >> $launcher
 echo "Name=SwapOff" >> $launcher
 echo "Comment=Turn off the swap" >> $launcher
 echo "Icon=drive-harddisk" >> $launcher
-echo "Exec=bash -c 'sleep 20 && pkexec swapoff.sh'" >> $launcher
+#echo "Exec=pkexec sudo /home/$USER/bin/swapoff.sh" >> $launcher
+echo "Exec=bash -c 'sleep 20 && pkexec sudo /home/petrstepanov/bin/swapoff.sh'" >> $launcher
 echo "Categories=Utility" >> $launcher
+echo "Terminal=true" >> $launcher
+#echo "X-GNOME-Autostart-enabled=true" >> $launcher
+#echo "X-GNOME-Autostart-Delay=20" >> $launcher
+
+# Tip: autostart with X-GNOME-.. not works, not specified in the desktop specification.
 
 chmod +x $launcher
