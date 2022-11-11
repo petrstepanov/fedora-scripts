6bcc28af2daa2468da97b2d877b608009acf7d7d
Petr Stepanov <stepanovps@gmail.com>
2022-11-11 15:15:26 -0800

    Laptop Lid fix

--- disable-suspend-lid-closed.sh
+++ disable-suspend-lid-closed.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sudo sed -i "s;HandleLidSwitch=suspend;HandleLidSwitch=ignore;" /etc/systemd/logind.conf
+sudo sed -i "s;#HandleLidSwitch=;HandleLidSwitch=;" /etc/systemd/logind.conf
+
 sudo sed -i "s;HandleLidSwitchExternalPower=suspend;HandleLidSwitchExternalPower=ignore;" /etc/systemd/logind.conf
+sudo sed -i "s;#HandleLidSwitchExternalPower=;HandleLidSwitchExternalPower=;" /etc/systemd/logind.conf
