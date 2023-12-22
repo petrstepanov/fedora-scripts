176e08362439e4d2ab5cc2b429780ec9f3ad875a
Petr Stepanov <stepanovps@gmail.com>
2023-12-21 16:19:05 -0800

    lol

--- change-terminal-sound.sh
+++ change-terminal-sound.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Change Drip in freedesktop sound scheme (used in terminal?)
-## Backup old sound 
-#sudo mv /usr/share/sounds/freedesktop/stereo/bell.oga /usr/share/sounds/freedesktop/stereo/bell.bak.oga
-#sudo cp /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga /usr/share/sounds/freedesktop/stereo/bell.oga
-
-# Change Drip in GNOME settings
-## Backup default sound
-sudo mv /usr/share/sounds/gnome/default/alerts/bark.ogg /usr/share/sounds/gnome/default/alerts/bark.bak.ogg
-sudo cp ./bark.ogg /usr/share/sounds/gnome/default/alerts/bark.ogg
+# Backup default sound
+sudo mv /usr/share/sounds/gnome/default/alerts/click.ogg /usr/share/sounds/gnome/default/alerts/click.bak.ogg
+sudo cp ./click.ogg /usr/share/sounds/gnome/default/alerts/click.ogg
 
 # Restart Gnome shell
 killall -3 gnome-shell
